0c129fffbcd98bec235f32047b34bc9643e926de
Dylan <d_h@mit.edu>
2020-11-09 21:06:47 -0600

    initial build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useCallback} from 'react';
+import Globe from './components/Globe';
+import './App.css';
+
+
+function App() {  
+  return (
+    <div className="App">
+      <Globe />
+    </div>
+  );
+}
+
+export default App;
