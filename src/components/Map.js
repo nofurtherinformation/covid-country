c288c85a589e93248f01dddf55d731c0b0691aad
Dylan <d_h@mit.edu>
2021-01-25 10:29:14 -0600

    Column Layer swap, performance, favicon

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import DeckGL from '@deck.gl/react';
 import {LightingEffect, DirectionalLight, AmbientLight } from '@deck.gl/core';
-import {SolidPolygonLayer, PolygonLayer } from '@deck.gl/layers';
+import {SolidPolygonLayer, ScatterplotLayer, PolygonLayer, ColumnLayer } from '@deck.gl/layers';
 import {COORDINATE_SYSTEM} from '@deck.gl/core';
 import {SimpleMeshLayer} from '@deck.gl/mesh-layers';
 import {SphereGeometry, IcoSphereGeometry} from '@luma.gl/core'
@@ -23,10 +23,6 @@ const MapContainer = styled.div`
     left:0;
     overflow:visible;
     background:rgb(15,15,15);
-    color:white;
-    button {
-        color:white;
-    }
 `
 
 const geom = new IcoSphereGeometry({
@@ -45,27 +41,31 @@ const ambientLight = new AmbientLight({
     intensity: 3
 });
 
+const colors = [
+    '#0D0887',
+    '#5C01A6',
+    '#9C179E',
+    '#CB4679',
+    '#ED7953',
+    '#FDB42F',
+    '#F0F921',
+    '#F0F921'
+]
+
+const breaks = [
+    0,
+    .03,
+    .05,
+    .10,
+    .15,
+    .20,
+    .25,
+    1000
+]
+
 const colorScale = d3.scaleLinear()
-    .domain([
-        0,
-        .03,
-        .05,
-        .10,
-        .15,
-        .20,
-        .25,
-        1000
-    ])
-    .range([
-        '#0D0887',
-        '#5C01A6',
-        '#9C179E',
-        '#CB4679',
-        '#ED7953',
-        '#FDB42F',
-        '#F0F921',
-        '#F0F921'
-    ])
+    .domain(breaks)
+    .range(colors)
 
 const handleColor = (val) => {
     let color = d3.color(colorScale(val))
@@ -75,6 +75,8 @@ const handleColor = (val) => {
 
 /* eslint-disable react/no-deprecated */
 const Map = () => {
+
+    const timeInterval = 175;
 
     const [initialViewState, setInitialViewState] = useState({
         latitude: 0,
@@ -90,39 +92,32 @@ const Map = () => {
         return [lightingEffect];
         });
     
-    const [currDate, setCurrDate] = useState(['2020-03-02'])
-    const [oldDate, setOldDate] = useState(['2020-03-01'])
+    const [currDate, setCurrDate] = useState(['2020-02-01'])
     const [timerId, setTimerId] = useState(null)
-    const [timerId2, setTimerId2] = useState(null)
     const [centroidData, setCentroidData] = useState([])
-    const [counter, setCounter] = useState(0)
 
     useEffect(() => {
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
         d3.csv(`${process.env.PUBLIC_URL}/data/centroids_testing_albers.csv`, d3.autoType)
             .then(data => { setCentroidData(data) })
     },[])
-    console.log(centroidData)
+    
     const PlayAnimation = () => {
-        setCurrDate(['2020-03-02'])
-        setOldDate(['2020-03-01'])
-        setTimerId(setInterval(() => {
-            setCurrDate(t => {
-                if (t < '2021-01-19') {
-                    var a = moment(t, 'YYYY-MM-DD')
-                    setOldDate(a.format().slice(0,10))
-                    a = a.add(1, 'day')
-                    return a.format().slice(0,10)
-                } else {
-                    setTimerId(null)
-                    setTimerId2(null)
-                    return t
-                }
-            })
-        }, 100))
-
-        setTimerId2(setInterval(() => {
-            setCounter( t => t >= 99 ? 0 : t+5)
-        },5))
+        setCurrDate(['2020-02-01'])
+        if (timerId === null) {
+            setTimerId(setInterval(() => {
+                setCurrDate(t => {
+                    if (t < '2021-01-19') {
+                        var a = moment(t, 'YYYY-MM-DD')
+                        a = a.add(1, 'day')
+                        return a.format().slice(0,10)
+                    } else {
+                        return t
+                    }
+                })
+            },timeInterval))
+        };
     }
 
     const layers =  [ 
@@ -160,36 +155,78 @@ const Map = () => {
             getLineColor: [240, 240, 240, 25],
             getLineWidth: 1
         }),
-        new SimpleMeshLayer({
-            id: 'mesh-layer',
+        // new SimpleMeshLayer({
+        //     id: 'mesh-layer',
+        //     coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
+        //     data: centroidData,
+        //     mesh: geom,
+        //     wireframe: true,
+        //     getPosition: d => [d.x,d.y],
+        //     getColor: d => {
+        //         let tempVal = d[`pos_${currDate}`]*(counter/100)+(d[`pos_${currDate}`]*((100-counter)/100))
+        //         return handleColor(tempVal)
+        //     },
+        //     getScale: d => [2500, 2500, (d[`pos_${currDate}`]*(counter/100)+(d[`pos_${currDate}`]*((100-counter)/100)))*500000],
+        //     getTranslation: d => [0, 0, (d[`pos_${currDate}`]*(counter/100)+(d[`pos_${currDate}`]*((100-counter)/100)))*500000],
+        //     // getScale: d => [(d[`tcap_${currDate}`]*(counter/100)+(d[`tcap_${currDate}`]*((100-counter)/100)))*20,
+        //     //                 (d[`tcap_${currDate}`]*(counter/100)+(d[`tcap_${currDate}`]*((100-counter)/100)))*20, 
+        //     //                 (d[`tcap_${currDate}`]*(counter/100)+(d[`tcap_${currDate}`]*((100-counter)/100)))*100], 
+        //     updateTriggers: {
+        //         getTranslation: [currDate],
+        //         getScale: [currDate, counter],
+        //         getColor: [currDate, counter]
+        //     }
+        // }),
+        new ColumnLayer({
+            id: 'line-layer',
             coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
             data: centroidData,
-            mesh: geom,
-            wireframe: true,
+            diskResolution: 6,
+            radius: 5000,
+            material:false,
+            extruded: true,
+            pickable: true,
+            elevationScale: 5000,
             getPosition: d => [d.x,d.y],
-            getColor: d => {
-                let tempVal = d[`pos_${currDate}`]*(counter/100)+(d[`pos_${currDate}`]*((100-counter)/100))
-                return handleColor(tempVal)
-            },
-            getScale: d => [2500, 2500, (d[`pos_${currDate}`]*(counter/100)+(d[`pos_${currDate}`]*((100-counter)/100)))*500000],
-            getTranslation: d => [0, 0, (d[`pos_${currDate}`]*(counter/100)+(d[`pos_${currDate}`]*((100-counter)/100)))*500000],
-            // getScale: d => [(d[`tcap_${currDate}`]*(counter/100)+(d[`tcap_${currDate}`]*((100-counter)/100)))*20,
-            //                 (d[`tcap_${currDate}`]*(counter/100)+(d[`tcap_${currDate}`]*((100-counter)/100)))*20, 
-            //                 (d[`tcap_${currDate}`]*(counter/100)+(d[`tcap_${currDate}`]*((100-counter)/100)))*100], 
+            getFillColor: d => handleColor(d[`pos_${currDate}`]),
+            getLineColor: [0, 0, 0],
+            getElevation: d => [(d[`pos_${currDate}`])*250],
             updateTriggers: {
-                getTranslation: [currDate],
-                getScale: [currDate, counter],
-                getColor: [currDate, counter]
+                getFillColor: currDate,
+                getElevation: currDate,
+            },
+            transitions: {
+                getFillColor: timeInterval,
+                getElevation: timeInterval,
             }
-        }),
+        })
     ]
 
     return (
         <MapContainer>
-            <Button onClick={PlayAnimation} id="playButton">Play</Button>
+            <Button onClick={PlayAnimation} id="playButton">{timerId === null ? 'Play' : 'Reset' }</Button>
             <div id="title">
                 <h1>COVID Testing Positivity Rates</h1>
                 <h2>7-Day Rolling Average of {currDate}</h2>
+            </div>
+            <div id="logo">
+                <a href="https://theuscovidatlas.org/" target="_blank" rel="noopener noreferrer">
+                    <img src={`${process.env.PUBLIC_URL}/logo.svg`} alt="US Covid Atlas Logo" />
+                </a>
+            </div>
+            <div id="attribution">
+                <a href="https://theuscovidatlas.org/" target="_blank" rel="noopener noreferrer">  
+                    <p>Learn more at USCovidAtlas.org</p>
+                </a>
+                <p>Testing data from CDC. State and County boundaries from US Census.</p>
+            </div>
+            <div id="scale">
+                <div id="breaks">
+                    {breaks.slice(1,-1).reverse().map((item, index) => <p>{index===0 ? '>' : ''}{parseInt(item*100)}%</p>)}
+                </div>
+                <div id="colors">
+                    {colors.slice(0,-1).reverse().map(item => <span style={{backgroundColor: item}}></span>)}
+                </div>
             </div>
             <DeckGL 
                 layers={layers} 
